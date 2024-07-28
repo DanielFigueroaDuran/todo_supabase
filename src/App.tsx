dada9171d844d5c83c6493c68edfa23272b16336
DanielFigueroaDuran <figueroadurandanielelias@gmail.com>
2024-07-28 20:32:28 +0200

    add styles components

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import TodoItem from "./components/TodoItem";
 
 function App() {
   return (
-    <main className="flex flex-col gap-4 mx-20 my-5">
-      <TodoItem title="Tarea 1" description="Urgente" isCompleted={true} />
-      <TodoItem title="Tarea 1" description="Urgente" isCompleted={true} />
-      <TodoItem title="Tarea 1" description="Urgente" isCompleted={true} />
+    <main className="flex items-center justify-center  mx-20 my-5">
+      <div className="flex flex-col gap-4 w-full max-w-xl">
+        <TodoItem title="Tarea 1" description="Urgente" isCompleted={true} />
+        <TodoItem title="Tarea 1" description="Urgente" isCompleted={true} />
+        <TodoItem title="Tarea 1" description="Urgente" isCompleted={true} />
+      </div>
     </main>
   );
 }
--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,3 +1,6 @@
+import { RiDeleteBinLine } from "react-icons/ri";
+import { CiEdit } from "react-icons/ci";
+
 interface Props {
   title: string;
   description: string;
@@ -6,11 +9,23 @@ interface Props {
 
 const TodoItem = ({ title, description, isCompleted }: Props) => {
   return (
-    <div className="flex gap-2 p-4 rounded-lg bg-slate-200">
+    <div className="flex items-center gap-2 p-4 rounded-lg bg-slate-200">
       <input type="checkbox" checked={isCompleted} />
-      <div>
-        <h4>{title}</h4>
-        <p>{description}</p>
+
+      <div className="flex justify-between flex-1 items-center">
+        <div>
+          <h4 className="text-2xl">{title}</h4>
+          <p className="text-slate-600">{description}</p>
+        </div>
+
+        <div className="flex gap-3">
+          <button className="bg-blue-600 p-2 rounded-md active:scale-90">
+            <CiEdit className=" text-white font-semibold" />
+          </button>
+          <button className="bg-red-600 p-2 rounded-md active:scale-90">
+            <RiDeleteBinLine className=" text-white font-semibold" />
+          </button>
+        </div>
       </div>
     </div>
   );
